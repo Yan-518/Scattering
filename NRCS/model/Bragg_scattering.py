47a2cfe27a57702620583439bc9baa6f636a84a2
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-07-15 00:26:58 +0200

    Update Bragg_scattering.py
    Include the cross-polarization model from Kudryavtsev, 2019
    Change the input

--- NRCS/model/Bragg_scattering.py
+++ NRCS/model/Bragg_scattering.py
@@ -2,8 +2,35 @@ from NRCS import constants as const
 from NRCS import spec
 from NRCS import spread
 import numpy as np
+from NRCS.spec.kudryavtsev05 import spec_peak
 
-def Bragg_scattering(k, kr, theta, azimuth, u_10, fetch, spec_name='elfouhaily'):
+def MSS(kbr, u_10, fetch):
+    # Kudryavtsev, 2019a
+    alpha = u_10*np.sqrt(spec_peak(u_10, fetch)/const.g)
+    kd = kbr / 4
+    return 4.5*10**(-3)*np.log(alpha**(-2)*kd*u_10**2/const.g)/2
+
+def CPBragg(kr, theta, azimuth, u_10, fetch, spec_name):
+    # [Kudryavtsev, 2019] equation A1c
+    nphi = theta.shape[0]
+    theta = theta.reshape(nphi,1)
+    eps_sin = np.sqrt(const.epsilon_sw-np.sin(theta)**2)
+    Gvv = np.cos(theta)**2*(const.epsilon_sw-1)*(const.epsilon_sw*(1+np.sin(theta)**2) - np.sin(theta)**2) / (const.epsilon_sw*np.cos(theta)+eps_sin)**2
+    Ghh = np.cos(theta)**2*(const.epsilon_sw-1)/(np.cos(theta)+eps_sin)**2
+    G = (np.abs(Gvv-Ghh)**2).reshape(nphi,1)
+    kbr = 2*kr*np.sin(theta)
+    sn2 = MSS(kbr, u_10, fetch).reshape(nphi,1)
+    specf = spec.models[spec_name]
+    if spec_name == 'elfouhaily':
+        # Directional spectrum model name
+        spreadf = spread.models[spec_name]
+        Bkdir = specf(kbr.reshape(nphi, 1), u_10, fetch) * spreadf(kbr.reshape(nphi, 1), azimuth, u_10, fetch) * kbr.reshape(nphi, 1)**3
+    else:
+        Bkdir = specf(kbr.reshape(nphi, 1), u_10, fetch, azimuth)
+    Brcp = np.pi * G * sn2 * Bkdir/(np.tan(theta)**4 * np.sin(theta)**2)
+    return Brcp
+
+def Bragg_scattering(k, kr, theta, azimuth, u_10, fetch, spec_name, polarization):
     """
     :param k:
     :param kr:
@@ -14,6 +41,10 @@ def Bragg_scattering(k, kr, theta, azimuth, u_10, fetch, spec_name='elfouhaily')
     :param spec_name:
     :return:
     """
+    if polarization == 'VH':
+        Br = CPBragg(kr, theta, azimuth, u_10, fetch, spec_name)
+        return Br.T
+
     nphi = theta.shape[0]
     nk = k.shape[0]
     nazi = azimuth.shape[0]
@@ -40,49 +71,43 @@ def Bragg_scattering(k, kr, theta, azimuth, u_10, fetch, spec_name='elfouhaily')
     ni = ni.reshape(nk, 1)
     ni = np.tan(ni)
     P = np.exp(-0.5 * (ni - np.mean(ni)) ** 2 / ni2) / np.sqrt(2 * np.pi * ni2)
-
+    #  the range of the sea surface slope
     angle_index = np.logical_and(-3 * 180 * np.arctan(np.sqrt(ni2)) / np.pi < np.arctan(ni) * 180 / np.pi, np.arctan(ni) * 180 / np.pi < 3 * 180 * np.arctan(np.sqrt(ni2)) / np.pi)
     P = P[angle_index]
     ni = ni[angle_index]
-#     P = P[np.arctan(ni)*180/np.pi>=0]
-#     ni = ni[np.arctan(ni)*180/np.pi>=0]
-    nnk = ni.shape[0]
-    ni = ni.reshape(nnk,1)
 
-    # # local incidence angle
-    # theta_l = np.abs(theta - np.arctan(ni).reshape(nk, 1))
-    # kbr = 2*kr*np.sin(theta_l)
+    nnk = ni.shape[0]
+    ni = ni.reshape(nnk, 1)
+
     # local incidence angle
     theta_l = np.abs(theta - np.arctan(ni).reshape(nnk, 1))
     kbr = 2*kr*np.sin(theta_l)
 
     # geometric scattering coefficients [Plant 1997] equation 5,6
     eps_sin = np.sqrt(const.epsilon_sw-np.sin(theta_l)**2)
-    Gvv = np.cos(theta_l)**2*(const.epsilon_sw-1)*(const.epsilon_sw*(1+np.sin(theta_l)**2) - np.sin(theta_l)**2) / (const.epsilon_sw*np.cos(theta_l)+eps_sin)**2
-    Gvv = np.abs(Gvv)**2
-    Ghh = np.cos(theta_l)**2*(const.epsilon_sw-1)/(np.cos(theta_l)+eps_sin)**2
-    Ghh = np.abs(Ghh)**2
 
     # 3-D Sk computed from kudryavtsev05
     if spec_name == 'kudryavtsev05':
         # Skk = np.zeros([nk, nphi, nazi])
 
         Skk = np.zeros([nnk, nphi, nazi])
-#         spec_Skk = specf(np.sort(kbr[0, :]).reshape(nphi, 1), u_10, fetch, azimuth) / np.sort(kbr[0, :]).reshape(nphi, 1) ** 4
+        # spec_Skk = specf(np.sort(kbr[0, :]).reshape(nphi, 1), u_10, fetch, azimuth) / np.sort(kbr[0, :]).reshape(nphi, 1) ** 4
         for nn in np.arange(nnk):
-            # sort_inc = np.argsort(kbr[nn, :])
             spec_Skk = specf(kbr[nn, :].reshape(nphi, 1), u_10, fetch, azimuth) / kbr[nn, :].reshape(nphi, 1) ** 4
-            # Skk[nn, :, :] = spec_Skk[sort_inc.astype(int), :]
             Skk[nn, :, :] = spec_Skk
-        # for nn in np.arange(nk):
-        #     Skk[nn, :, :] = (specf(kbr[nn, :].reshape(nphi, 1), u_10, fetch, azimuth) / kbr[nn, :].reshape(nphi, 1) ** 4)  # equation 45
         inc = np.where(azimuth >= 0)[0]
         incc = np.linspace(1, inc[0], inc[0])
         inc = np.hstack((inc, incc))
         Skk_pi = Skk[:, :, inc.astype(int)]
 
-    Br_VV = np.zeros([nazi, nphi])
-    Br_HH = np.zeros([nazi, nphi])
+    if polarization == 'VV':
+        G = np.cos(theta_l)**2*(const.epsilon_sw-1)*(const.epsilon_sw*(1+np.sin(theta_l)**2) - np.sin(theta_l)**2) / (const.epsilon_sw*np.cos(theta_l)+eps_sin)**2
+        G = np.abs(G)**2
+    else:
+        G = np.cos(theta_l)**2*(const.epsilon_sw-1)/(np.cos(theta_l)+eps_sin)**2
+        G = np.abs(G)**2
+
+    Br = np.zeros([nazi, nphi])
     for num in np.arange(nazi):
          #  wave number folded folded spcetrum of the surface elevations
         if spec_name == 'elfouhaily':
@@ -96,34 +121,20 @@ def Bragg_scattering(k, kr, theta, azimuth, u_10, fetch, spec_name='elfouhaily')
         Skb_r = (Skb+Skb_pi) / 2 # Kudryavtsev 2003a equation 2
 
     # pure Bragg scattering NRCS
-        br0_vv = 16*np.pi*kr**4*Gvv*Skb_r
-        br0_hh = 16*np.pi*kr**4*Ghh*Skb_r
-
-    # # Bragg scattering composite model
-    #     BR_vv = br0_vv*P.reshape(nk, 1)
-    #     BR_hh = br0_hh*P.reshape(nk, 1)
+        br0 = 16 * np.pi * kr ** 4 * G * Skb_r
 
     # Bragg scattering composite model
-        BR_vv = br0_vv * P.reshape(nnk, 1)
-        BR_hh = br0_hh * P.reshape(nnk, 1)
+        BR = br0 * P.reshape(nnk, 1)
 
     # integral over kbr >= kd
-        VV = []
-        HH = []
+        intebr = []
 
         for i in np.arange(nphi):
             a = np.tan(theta[i]-const.d / 2)
             b = np.tan(theta[i]+const.d / 2)
-            # inte_vv = BR_vv[:, i].reshape(nk, 1)
-            # inte_hh = BR_hh[:, i].reshape(nk, 1)
-            inte_vv = BR_vv[:, i].reshape(nnk, 1)
-            inte_hh = BR_hh[:, i].reshape(nnk, 1)
-            vv = np.trapz(inte_vv[ni <= a], ni[ni <= a])+np.trapz(inte_vv[ni >= b], ni[ni >= b])
-            hh = np.trapz(inte_hh[ni <= a], ni[ni <= a])+np.trapz(inte_hh[ni >= b], ni[ni >= b])
-            VV.append(vv)
-            HH.append(hh)
-        VV = np.asarray(VV)
-        HH = np.asarray(HH)
-        Br_VV[num, :] = VV
-        Br_HH[num, :] = HH
-    return Br_VV, Br_HH
+            inte = BR[:, i].reshape(nnk, 1)
+            vv = np.trapz(inte[ni <= a], ni[ni <= a]) + np.trapz(inte[ni >= b], ni[ni >= b])
+            intebr.append(vv)
+        intebr = np.asarray(intebr)
+        Br[num, :] = intebr
+    return Br
