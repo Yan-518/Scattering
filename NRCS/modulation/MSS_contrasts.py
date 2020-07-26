dc3d788bf6ae99e354dceb0a316067402b7e3c2e
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-07-27 00:32:52 +0200

    Update MSS_contrasts.py
    Add empirical MSS contrasts

--- NRCS/modulation/MSS_contrasts.py
+++ NRCS/modulation/MSS_contrasts.py
@@ -1,5 +1,14 @@
 import numpy as np
+import NRCS.constants as const
 from NRCS.modulation import Spectrum
+from NRCS.spec.kudryavtsev05 import fv
+
+def MSS_em(K, u_10, tsc):
+    # Empirical mss from Eq.17 in [Kudryavtsev, 2012]
+    divergence = np.gradient(tsc[:, :, 0], 1e3, axis=1) + np.gradient(tsc[:, :, 1], 1e3, axis=0)
+    cs = 180
+    U = fv(u_10)
+    return -cs * divergence / (U * np.sqrt(const.ky * K))
 
 def MSS(k, K, u_10, fetch, azimuth, spec_name, tsc):
     B_old, B_new = Spectrum(k, K, u_10, fetch, azimuth, spec_name, tsc)
