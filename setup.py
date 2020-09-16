dbf40d84e70e8701e21d94be1c17b96a32243add
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-09-16 14:59:28 +0200

    Update setup.py
    Include bistatic

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ setup(
     name='NRCS',
     version='20.01',
     packages=['NRCS',
+              'NRCS.bistatic',
               'NRCS.constants',
-#               'NRCS.constants',
+              'NRCS.current',
+              'NRCS.LUT',
               'NRCS.model',
               'NRCS.spec',
               'NRCS.spread',
               'NRCS.modulation',
               'NRCS.validation'],
     # url='https://github.com/Yan-518/NRCS.git',
-    author='Yan Yuan and other',
+    author='Yan Yuan and others',
     author_email='y826302970y@outlook.com',
     description='',
-    install_requires=['numpy', 'scipy', 'matplotlib']
+    install_requires=['numpy', 'scipy', 'matplotlib', 'stereoid', 'drama']
 )
