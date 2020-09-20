d12b995b386b290f71da65ae3d050dec29b31fab
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-09-20 16:49:47 +0200

    Update Br_bi.py
    Change the input for elfouhaily from radians to degrees

--- NRCS/bistatic/Br_bi.py
+++ NRCS/bistatic/Br_bi.py
@@ -130,10 +130,10 @@ def Br_bi(k, kr, theta_i, theta_s, theta_eq, eq_azi, u_10, fetch, spec_name, pol
         poli = 0
     bist_ang_az = 2 * eq_azi
     # polarization for the mono-static equivalent
-    (rot_ang_1, rot_ang_2, rot_ang_tot, Ps1, Ps2, Ps_tot) = elfouhaily(poli, eq_azi, theta_i, eq_azi, theta_i)
-    Ps_tot_eq_norm = np.linalg.norm(Ps_tot, axis=-1)
-    (rot_ang_1, rot_ang_2, rot_ang_tot, Ps1, Ps2, Ps_tot) = elfouhaily(poli, 0, theta_i, bist_ang_az, theta_s)
-    Ps_tot_bi_norm = np.linalg.norm(Ps_tot, axis=-1)
+    (rot_ang_1, rot_ang_2, rot_ang_tot, Ps1, Ps2, Ps_tot) = elfouhaily(poli, np.degrees(eq_azi), np.degrees(theta_i), np.degrees(eq_azi), np.degrees(theta_i))
+    Ps_eq_norm = np.linalg.norm(Ps_tot, axis=-1)
+    (rot_ang_1, rot_ang_2, rot_ang_tot, Ps1, Ps2, Ps_tot) = elfouhaily(poli, 0, np.degrees(theta_i), np.degrees(bist_ang_az), np.degrees(theta_s))
+    Ps_bi_norm = np.linalg.norm(Ps_tot, axis=-1)
     #  transfer function
     M = Ps_tot_bi_norm ** 2 / Ps_tot_eq_norm ** 2
 
