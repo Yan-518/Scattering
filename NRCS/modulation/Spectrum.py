15add2b2b63865183d75f49fda5e9e1d6a67e994
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-10-16 19:12:00 +0200

    Update Spectrum.py

--- NRCS/modulation/Spectrum.py
+++ NRCS/modulation/Spectrum.py
@@ -1,8 +1,21 @@
 import numpy as np
 from NRCS import constants as const
-from NRCS import spec
-from NRCS import spread
+from NRCS.spec import kudryavtsev05
 from NRCS.spec.kudryavtsev05 import fv
+
+def sample_back(k, u_10, tsc):
+    nk = k.shape[2]
+    ax0 = np.linspace(0, tsc.shape[0], k.shape[0] + 1)
+    ax1 = np.linspace(0, tsc.shape[1], k.shape[1] + 1)
+    k_re = np.zeros([tsc.shape[0], tsc.shape[1], nk])
+    wind = np.zeros([tsc.shape[0], tsc.shape[1]])
+    for ii in np.arange(k.shape[0]):
+        for jj in np.arange(k.shape[1]):
+            k_re[int(ax0[ii]):int(ax0[ii + 1]), int(ax1[jj]):int(ax1[jj + 1]), :] = k[ii, jj, :] * np.ones(
+                [int(tsc.shape[0] / k.shape[0]), int(tsc.shape[1] / k.shape[1]), nk])
+            wind[int(ax0[ii]):int(ax0[ii + 1]), int(ax1[jj]):int(ax1[jj + 1])] = u_10[ii, jj] * np.ones(
+                [int(tsc.shape[0] / k.shape[0]), int(tsc.shape[1] / k.shape[1])])
+    return k_re, wind
 
 def wind_exp(k):
     # m*
@@ -17,22 +30,14 @@ def wn_dless(k, u_10):
     #     dimensionless wave number of wind waves
     return k*fv(u_10)[:, :, None]**2/const.g
 
-def wn_dless_single(k, u_10):
-    #     dimensionless wave number of wind waves
-    return k*fv(u_10)**2/const.g
-
-def wn_exp(k, u_10, fetch, azimuth, spec_name):
+def wn_exp(k, u_10, fetch, azimuth):
     # mk
     # wave number exponent of the omnidirirectional spectrum of the wave action
     nk = k.shape[0]
     omega = np.sqrt(const.g * k + const.gamma * k ** 3 / const.rho_water)  # intrinstic frequency from dispersion relation
     # Omni directional spectrum model name
-    specf = spec.models[spec_name]
-    if spec_name == 'elfouhaily':
-        Sk = specf(k.reshape(nk, 1), u_10, fetch)
-    else:
-        Sk = specf(k.reshape(nk, 1), u_10, fetch, azimuth) * k.reshape(nk, 1) ** 4 # equation 45
-        Sk = np.trapz(Sk, azimuth, axis=1)
+    Sk = kudryavtsev05(k.reshape(nk, 1), u_10, fetch, azimuth) * k.reshape(nk, 1) ** 4 # equation 45
+    Sk = np.trapz(Sk, azimuth, axis=1)
     N = omega.reshape(nk, 1) * Sk.reshape(nk, 1) / k.reshape(nk, 1) ** 2
     mk = k.reshape(nk, 1) * np.gradient(np.log(N[:, 0]), k[1, 0] - k[0, 0]).reshape(nk, 1)
     return mk
@@ -40,58 +45,54 @@ def wn_exp(k, u_10, fetch, azimuth, spec_name):
 def Trans(k, K, u_10, fetch, azimuth, tsc):
     nk = k.shape[2]
     c_beta = 0.04  # wind wave growth parameter
-    K_hat = K*fv(u_10)**2/const.g
+    k_re, wind = sample_back(k, u_10, tsc)
+    k_hat = wn_dless(k_re, wind)
+    K_hat = K * fv(wind) ** 2 / const.g
     ax0 = np.linspace(0, tsc.shape[0], k.shape[0] + 1)
     ax1 = np.linspace(0, tsc.shape[1], k.shape[1] + 1)
     wind_exponent = np.zeros([tsc.shape[0], tsc.shape[1], nk])
     mk = np.zeros([tsc.shape[0], tsc.shape[1], nk])
-    k_hat = np.zeros([tsc.shape[0], tsc.shape[1], nk])
     for ii in np.arange(k.shape[0]):
         for jj in np.arange(k.shape[1]):
             wind_expo = wind_exp(k[ii, jj, :])
             wind_exponent[int(ax0[ii]):int(ax0[ii + 1]), int(ax1[jj]):int(ax1[jj + 1]), :] = wind_expo * np.ones(
             [int(tsc.shape[0] / k.shape[0]), int(tsc.shape[1] / k.shape[1]), nk])
-            mm = wn_exp(k[ii, jj, :].reshape(nk, 1), u_10[ii, jj], fetch, azimuth, 'kudryavtsev05')[:, 0]
+            mm = wn_exp(k[ii, jj, :].reshape(nk, 1), u_10[ii, jj], fetch, azimuth)[:, 0]
             mk[int(ax0[ii]):int(ax0[ii + 1]), int(ax1[jj]):int(ax1[jj + 1]), :] = mm * np.ones(
                 [int(tsc.shape[0] / k.shape[0]), int(tsc.shape[1] / k.shape[1]), nk])
-            k_min = spec_peak(u_10[ii, jj], fetch)
-            k_hat[ii, jj, :] = np.linspace(k_min, const.ky, nk)
+
     c_tau = wind_exponent / (2 * c_beta)  # constant
-    k_hat = wn_dless(k_hat, u_10)
     # divergence of the sea surface current
     divergence = np.gradient(tsc[:, :, 0], 1e3, axis=1) + np.gradient(tsc[:, :, 1], 1e3, axis=0)
     # transfer function
-    T = c_tau * k_hat ** (-3 / 2) * mk * fv(u_10)[:, :, None] * divergence[:, :, None] / (const.g*(
+    T = c_tau * k_hat ** (-3 / 2) * mk * fv(wind)[:, :, None] * divergence[:, :, None] / (const.g*(
                 1 + 1j * c_tau * k_hat ** (-2) * K_hat[:, :, None]))
     return T
 
-def Trans_single(k, K, u_10, fetch, azimuth, spec_name, divergence):
-    nk = k.shape[0]
-    c_beta = 0.04  # wind wave growth parameter
-    k_hat = wn_dless_single(k, u_10)
-    K_hat = K*fv(u_10)**2/const.g
-    wind_exponent = wind_exp(k)
-    mk = wn_exp(k.reshape(nk, 1), u_10, fetch, azimuth, spec_name)
-    c_tau = wind_exponent / (2 * c_beta)  # constant
-    # transfer function
-    T = c_tau * k_hat ** (-3 / 2) * mk * fv(u_10) * divergence / (const.g*(1 + 1j * c_tau * k_hat ** (-2) * K_hat))
-    return T
+# def Trans_single(k, K, u_10, fetch, azimuth, spec_name, divergence):
+#     nk = k.shape[0]
+#     c_beta = 0.04  # wind wave growth parameter
+#     k_hat = wn_dless_single(k, u_10)
+#     K_hat = K*fv(u_10)**2/const.g
+#     wind_exponent = wind_exp(k)
+#     mk = wn_exp(k.reshape(nk, 1), u_10, fetch, azimuth, spec_name)
+#     c_tau = wind_exponent / (2 * c_beta)  # constant
+#     # transfer function
+#     T = c_tau * k_hat ** (-3 / 2) * mk * fv(u_10) * divergence / (const.g*(1 + 1j * c_tau * k_hat ** (-2) * K_hat))
+#     return T
 
-def Spectrum(k, K, u_10, fetch, azimuth, spec_name, tsc):
+def Spectrum(k, K, u_10, fetch, azimuth, tsc, wind_dir):
     nk = k.shape[2]
-    nazi = azimuth.shape[0]
     T = Trans(k, K, u_10, fetch, azimuth, tsc)
-    B_old = np.zeros([k.shape[0], k.shape[1], nk, nazi])
-    specf = spec.models[spec_name]
-    if spec_name == 'elfouhaily':
-        spreadf = spread.models[spec_name]
-        for ii in np.arange(k.shape[0]):
-            for jj in np.arange(k.shape[1]):
-                B_old[ii, jj, :, :] = specf(k[ii, jj, :].reshape(nk, 1), u_10[ii, jj], fetch) * spreadf(k[ii, jj, :].reshape(nk, 1), azimuth, u_10[ii, jj], fetch) * k[ii, jj, :].reshape(nk,1) ** 3
-    else:
-        for ii in np.arange(k.shape[0]):
-            for jj in np.arange(k.shape[1]):
-                B_old[ii, jj, :, :] = specf(k[ii, jj, :].reshape(nk, 1), u_10[ii, jj], fetch, azimuth)
-
-    B_new = B_old*(1+np.abs(T))
+    ind = np.where(np.degrees(azimuth) == wind_dir)[0]
+    ax0 = np.linspace(0, tsc.shape[0], k.shape[0] + 1)
+    ax1 = np.linspace(0, tsc.shape[1], k.shape[1] + 1)
+    B_old = np.zeros([tsc.shape[0], tsc.shape[1], nk])
+    for ii in np.arange(k.shape[0]):
+        for jj in np.arange(k.shape[1]):
+            B_old[int(ax0[ii]):int(ax0[ii + 1]), int(ax1[jj]):int(ax1[jj + 1]), :] = kudryavtsev05(
+                k[ii, jj, :].reshape(nk, 1), u_10[ii, jj], fetch, azimuth)[:, ind][:, 0] * np.ones(
+                [int(tsc.shape[0] / k.shape[0]), int(tsc.shape[1] / k.shape[1]), nk])
+    B_new = B_old * (1 + np.abs(T))
     return B_old, B_new
+
