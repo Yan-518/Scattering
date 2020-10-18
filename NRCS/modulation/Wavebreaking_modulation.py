b80b33e5c6d7b564edd97b1c0bee74718cc2ea15
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-10-18 23:00:31 +0200

    Add files via upload
    Add wave breaking modulation code

---
+++ NRCS/modulation/Wavebreaking_modulation.py
@@ -0,0 +1,59 @@
+import numpy as np
+from NRCS import constants as const
+from NRCS.spec import kudryavtsev05
+from NRCS.spec.kudryavtsev05 import param
+from NRCS.spec.kudryavtsev05 import spec_peak
+from NRCS.modulation.Bragg_modulation import Trans_func
+
+def Wavebreaking_modulation(kr, K, theta, azimuth, u_10, fetch, wind_dir, tsc, polarization):
+    """
+    :param kp:
+    :param kr:
+    :param theta:
+    :param azi:
+    :param u_10:
+    :param fetch:
+    :return:
+    """
+    if polarization == 'VH':
+        print('no modulation cross-pol')
+        return
+
+    nphi = theta.shape[0]
+
+    ind = np.where(np.degrees(azimuth) == wind_dir)[0]
+
+    # divergence of the sea surface current
+    divergence = np.gradient(tsc[:, :, 0], 1e3, axis=1) + np.gradient(tsc[:, :, 1], 1e3, axis=0)
+
+    # NRCS of plumes
+    wb0 = np.exp(-np.tan(theta)**2/const.Swb)/(np.cos(theta)**4*const.Swb)+const.yitawb/const.Swb # Kudryavstev 2003a equation (60)
+    knb = min(const.br*kr, const.kwb)
+
+    # tilting transfer function
+    dtheta = theta[1]-theta[0]
+    Mwb = np.gradient(wb0, dtheta)/wb0
+
+    # distribution function
+    # in radians azimuth of breaking surface area: -pi/2,pi/2
+    # phi1 = np.linspace(-np.pi/2, np.pi/2, nphi)
+    phi1 = np.linspace(-np.pi, np.pi, nphi)
+    nk = 1024
+
+    q = np.zeros([tsc.shape[0],tsc.shape[1]])
+    WB = np.zeros([tsc.shape[0],tsc.shape[1]])
+
+    for ii in np.arange(tsc.shape[0]):
+        for jj in np.arange(tsc.shape[1]):
+            KK = np.linspace(10 * spec_peak(u_10[ii,jj], fetch), knb, nk)
+            T = Trans_func(KK, K[ii, jj], u_10[ii, jj], fetch, azimuth, divergence[ii, jj])
+            Bkdir = kudryavtsev05(KK.reshape(nk, 1), u_10[ii, jj], fetch, phi1)* (1+abs(T.reshape(nk,1)))
+            n, alpha = param(KK, u_10[ii, jj], fetch)
+            lamda = (Bkdir/alpha.reshape(nk, 1))**(n.reshape(nk, 1)+1)/(2*KK.reshape(nk, 1)) # distribution of breaking front lengths
+            lamda_k = np.trapz(lamda, phi1, axis=1)
+            lamda = np.trapz(lamda, KK, axis=0)
+            lamda_k = np.trapz(lamda_k, KK)
+            q[ii, jj] = const.cq * lamda_k
+            Awb = np.trapz(np.cos(phi1 - azimuth[ind]) * lamda, phi1) / lamda_k
+            WB[ii, jj] = wb0[jj]*(1+Mwb[jj]*const.theta_wb*Awb)
+    return WB, q
