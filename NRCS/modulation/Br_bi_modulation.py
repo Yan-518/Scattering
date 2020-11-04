8b1cf014a43fa2d800dc4799856a6399b7b17cd4
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-11-05 00:51:56 +0100

    Update Br_bi_modulation.py

--- NRCS/modulation/Br_bi_modulation.py
+++ NRCS/modulation/Br_bi_modulation.py
@@ -63,8 +63,8 @@ def eq_br_mo(k, K, kr, theta_eq, bist_ang_az, eq_azi, wind_dir, u_10, fetch, div
             kbr = 2 * kr * np.sin(theta_l) * np.cos(bist_ang_az[jj]/2)
             kkbr = np.sort(kbr)
             T = Trans_func(kkbr[:, 0], K[ii, jj], u_10[ii, jj], fetch, azimuth, div[ii, jj])[np.argsort(kbr[:,0])]
-            Skb = B_int_single(kkbr.reshape(nnk, 1), u_10[ii, jj], fetch, eq_azi[jj])[np.argsort(kbr)[:,0], :] * (1+abs(T)) / kbr.reshape(nnk,1) ** 4
-            Skb_pi = B_int_single(kkbr.reshape(nnk, 1), u_10[ii, jj], fetch, np.pi + eq_azi[jj])[np.argsort(kbr)[:,0], :] * (1+abs(T)) / kbr.reshape(nnk, 1) ** 4
+            Skb = B_int_single(kkbr.reshape(nnk, 1), u_10[ii, jj], fetch, eq_azi[jj])[np.argsort(kbr[:,0]), :] * (1+abs(T)) / kbr.reshape(nnk,1) ** 4
+            Skb_pi = B_int_single(kkbr.reshape(nnk, 1), u_10[ii, jj], fetch, np.pi + eq_azi[jj])[np.argsort(kbr[:,0]), :] * (1+abs(T)) / kbr.reshape(nnk, 1) ** 4
             Skb_r = (Skb + Skb_pi) / 2  # Kudryavtsev 2003a equation 2
             if polarization == 'VV':
                 G = np.cos(theta_l) ** 2 * (const.epsilon_sw - 1) * (
