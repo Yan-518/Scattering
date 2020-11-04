acc40c16d62037a183c18fc405b0345f38d5fbed
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-11-04 17:40:53 +0100

    Update Wb_bi_modulation.py

--- NRCS/modulation/Wb_bi_modulation.py
+++ NRCS/modulation/Wb_bi_modulation.py
@@ -43,7 +43,7 @@ def eq_wb_mo(kr, K, theta_eq, eq_azi, u_10, fetch, div):
             lamda = np.trapz(lamda, KK, axis=0)
             lamda_k = np.trapz(lamda_k, KK)
             q[ii, jj] = const.cq * lamda_k
-            Awb = np.trapz(np.cos(phi1 - eq_azi[jj]) * lamda, phi1, axis=1) / lamda_k
+            Awb = np.trapz(np.cos(phi1 - eq_azi[jj]) * lamda, phi1) / lamda_k
             WB[ii, jj] = wb0 * (1 + Mwb * const.theta_wb * Awb)
     return WB, q
 
