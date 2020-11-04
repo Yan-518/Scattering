91900e67d255d8bfc9911db1fb5099bf83abb472
Yan-518 <56545813+Yan-518@users.noreply.github.com>
2020-11-05 00:40:03 +0100

    Update const_brmo.py

--- NRCS/modulation/const_brmo.py
+++ NRCS/modulation/const_brmo.py
@@ -54,8 +54,8 @@ def const_brmo(k, K, kr, theta, azimuth, u_10, fetch, wind_dir, ind_pi, div, pol
             # geometric scattering coefficients [Plant 1997] equation 5,6
             eps_sin = np.sqrt(const.epsilon_sw-np.sin(theta_l)**2)
             kkbr = np.sort(kbr)
-            T = Trans_func(kkbr[:, 0], K[ii, jj], u_10, fetch, azimuth, div[ii, jj])[np.argsort(kbr)]
-            spec_Skk = kudryavtsev05(kkbr.reshape(nnk, 1), u_10, fetch, azimuth)[np.argsort(kbr)[:,0], :] * (1+abs(T)) / kbr.reshape(nnk, 1) ** 4
+            T = Trans_func(kkbr[:, 0], K[ii, jj], u_10, fetch, azimuth, div[ii, jj])[np.argsort(kbr[:,0])]
+            spec_Skk = kudryavtsev05(kkbr.reshape(nnk, 1), u_10, fetch, azimuth)[np.argsort(kbr[:,0]), :] * (1+abs(T)) / kbr.reshape(nnk, 1) ** 4
             Skb_r = (spec_Skk[:, ind] + spec_Skk[:, ind_pi]) / 2
             if polarization == 'VV':
                 G = np.cos(theta_l)**2*(const.epsilon_sw-1)*(const.epsilon_sw*(1+np.sin(theta_l)**2) - np.sin(theta_l)**2) / (const.epsilon_sw*np.cos(theta_l)+eps_sin)**2
